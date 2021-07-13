5910763edb966c86de0b80cc7d4331cd5d30c3fa
linh <linh.ngo@bnksolution.com>
2021-07-13 13:54:24 +0700

    Using settter to change duck behavior at runtime

---
+++ src/com/codewithlinh/ModelDuck.java
@@ -0,0 +1,13 @@
+package com.codewithlinh;
+
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a model duck");
+    }
+}
